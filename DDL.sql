89b661b08ccb1f8c8f59b4da6967671b9ede9b33
ViviMac5 <mcintyvi@oregonstate.edu>
2024-05-11 23:21:21 -0700

    updated DDL for all R_has_P changes

--- DDL.sql
+++ DDL.sql
@@ -8,6 +8,7 @@ SET FOREIGN_KEY_CHECKS=0;
 SET AUTOCOMMIT = 0;
 
 
+-- Create Employees table
 CREATE OR REPLACE TABLE Employees (
     employee_id int(11) NOT NULL AUTO_INCREMENT, 
     first_name varchar(145) NOT NULL,
@@ -16,6 +17,7 @@ CREATE OR REPLACE TABLE Employees (
     PRIMARY KEY(employee_id)
 );
 
+-- Create Participants table
 CREATE OR REPLACE TABLE Participants (
     participant_id int(11) NOT NULL AUTO_INCREMENT, 
     first_name varchar(145) NOT NULL,
@@ -26,18 +28,20 @@ CREATE OR REPLACE TABLE Participants (
     PRIMARY KEY (participant_id)
 );
 
+-- Create Programs table
 CREATE OR REPLACE TABLE Programs(
     program_id int(11) NOT NULL AUTO_INCREMENT, 
     name varchar(145) NOT NULL, 
     capacity int(11) NOT NULL, 
     location varchar(145) NOT NULL, 
     date_time datetime NOT NULL,  
-    Employee_id int, 
+    employee_id int, 
     PRIMARY KEY (program_id), 
-    FOREIGN KEY (Employee_id) REFERENCES Employees(employee_id)
+    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
     ON DELETE CASCADE
 ); 
 
+-- Create Campgrounds table
 CREATE OR REPLACE TABLE Campgrounds (
     campground_id int(11) NOT NULL AUTO_INCREMENT,
     campground_name varchar(145),
@@ -45,92 +49,125 @@ CREATE OR REPLACE TABLE Campgrounds (
     PRIMARY KEY (campground_id)
 );
 
+-- Create Reservations table
 CREATE OR REPLACE TABLE Reservations (
     reservation_id int(11) NOT NULL AUTO_INCREMENT,
-    Employee_id int(11) NOT NULL,
-    date_created date NOT NULL,
-    Program_id int(11),
+    employee_id int(11) NOT NULL,
+    date_time_created datetime NOT NULL,
+    program_id int(11),
     is_campground tinyint(1) NOT NULL,
-    Campground_id int(11),
+    campground_id int(11),
     camping_start_date date,
     camping_end_date date,
     PRIMARY KEY (reservation_id),
-    FOREIGN KEY (Employee_id) REFERENCES Employees(employee_id),
-    FOREIGN KEY (Campground_id) REFERENCES Campgrounds(campground_id),
-    FOREIGN KEY (Program_id) REFERENCES Programs(program_id)
+    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
     ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (campground_id) REFERENCES Campgrounds(campground_id)
+    ON DELETE SET NULL
+    ON UPDATE SET NULL,
+    FOREIGN KEY (program_id) REFERENCES Programs(program_id)
+    ON DELETE SET NULL
+    ON UPDATE SET NULL
 );
 
-CREATE OR REPLACE TABLE Reservations_has_Participants(
-    Reservation_id int(11) NOT NULL,
-    Participant_id int(11) NOT NULL,
-    CONSTRAINT PRIMARY KEY (Reservation_id, Participant_id),
-    CONSTRAINT FOREIGN KEY (Reservation_id) REFERENCES Reservations (reservation_id),
-    CONSTRAINT FOREIGN KEY (Participant_id) REFERENCES Participants (participant_id)
+-- Create Reservations_has_Participants table
+CREATE OR REPLACE TABLE Reservations_has_Participants (
+    reservation_participant_id int(11) NOT NULL AUTO_INCREMENT,
+    reservation_id int(11) NOT NULL,
+    participant_id int(11) NOT NULL,
+    PRIMARY KEY (reservation_participant_id),
+    FOREIGN KEY (reservation_id) REFERENCES Reservations (reservation_id)
     ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (participant_id) REFERENCES Participants (participant_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
+
+-- Insert sample data for Employees
 INSERT INTO Employees (first_name, last_name, phone_number)
     VALUES
     ('Viviana', 'McIntyre', '505-987-6543'),
     ('Noel', 'Freeze', '505-876-5467'),
     ('Jake', 'Santana', '505-716-8975');
 
--- Campground data retrieived from: https://www.nps.gov/yose/planyourvisit/campgrounds.htm
-
+-- Insert sample data for Participants
 INSERT INTO Participants (first_name, last_name, age, phone_number, email)
     VALUES 
         ('Andrew', 'Kent', 23, '564-798-0908', 'andrew_kent@gmail.com'),
         ('Sharon', 'Myers', 45, '961-893-0000', 'm_sharon@yahoo.com'),
         ('Becky', 'Hendon', 19, '351-098-3289', 'bhendon@outlook.com');
 
+-- Insert sample data for Campgrounds
+-- Campground data retrieived from: https://www.nps.gov/yose/planyourvisit/campgrounds.htm
 INSERT INTO Campgrounds (campground_name, num_campsites)
     VALUES
     ('Upper Pines', 238),
     ('Lower Pines', 60),
     ('North Pines', 81);
 
-INSERT INTO Programs(name, capacity, location, date_time, Employee_id)
+-- Insert sample data for Programs
+INSERT INTO Programs(name, capacity, location, date_time, employee_id)
     VALUES 
         ('Outdoor Restorative Yoga', 25,'Lower Pines', '2024-05-03 12:00:00', (SELECT employee_id FROM Employees WHERE first_name = 'Viviana' and last_name = 'McIntyre')), 
         ('Community hike', 17, 'North Pines', '2024-05-15 09:00:00', (SELECT employee_id FROM Employees WHERE first_name = 'Noel' and last_name = 'Freeze')), 
         ('Park Tour', 30, "Upper Pines", '2024-06-01 10:00:00', (SELECT employee_id FROM Employees WHERE first_name = 'Jake' and last_name = 'Santana')); 
 
-INSERT INTO Reservations (date_created, is_campground, Campground_id, Program_id, 
-Employee_id, camping_start_date, camping_end_date)
+-- Insert sample data for Reservations
+INSERT INTO Reservations (date_time_created, is_campground, campground_id, program_id, 
+employee_id, camping_start_date, camping_end_date)
     VALUES
-    ("2023-10-15", 1, 
+    ('2023-10-15 09:09:09', 1, 
         NULL, (SELECT program_id FROM Programs WHERE name = 'Outdoor Restorative Yoga'), 
         (SELECT employee_id FROM Employees WHERE first_name = 'Viviana' and last_name = 'McIntyre'),
         NULL, NULL),
-    ("2023-12-06", 1,
+    ('2023-12-06 08:07:06', 1,
         (SELECT campground_id FROM Campgrounds WHERE campground_name = 'Lower Pines'), NULL,
         (SELECT employee_id FROM Employees WHERE first_name = 'Jake' and last_name = 'Santana'),
         "2024-05-04", "2024-05-11"),
-    ("2024-03-18", 1,
+    ('2024-03-18 10:05:02', 1,
         NULL, (SELECT program_id FROM Programs WHERE name = 'Park Tour'),
         (SELECT employee_id FROM Employees WHERE first_name = 'Noel' and last_name = 'Freeze'),
         NULL, NULL),
-    ("2024-02-03", 1,
+    ('2024-02-03 03:03:03', 1,
         (SELECT campground_id FROM Campgrounds WHERE campground_name = 'North Pines'), NULL,
         (SELECT employee_id FROM Employees WHERE first_name = 'Noel' and last_name = 'Freeze'),
         "2024-07-25", "2024-07-28");
 
-INSERT INTO Reservations_has_Participants (Reservation_id, Participant_id)
+-- Insert sample data for Reservations_has_Participants
+INSERT INTO Reservations_has_Participants (reservation_id, participant_id)
     VALUES 
         ((SELECT reservation_id FROM Reservations
-            WHERE camping_start_date = '2024-05-04' and camping_end_date = '2024-05-11'),
-        (SELECT participant_id FROM Participants 
-            WHERE first_name = 'Andrew' and last_name = 'Kent' and phone_number = '564-798-0908'));
+            WHERE employee_id = 3 and date_time_created = '2023-12-06 08:07:06'),
+            (SELECT participant_id FROM Participants 
+                WHERE first_name = 'Andrew' and last_name = 'Kent' and phone_number = '564-798-0908')),
+        ((SELECT reservation_id FROM Reservations
+            WHERE employee_id = 1 and date_time_created = '2023-10-15 09:09:09'),
+            (SELECT participant_id FROM Participants 
+                WHERE first_name = 'Sharon' and last_name = 'Myers' and phone_number = '961-893-0000')),
+        ((SELECT reservation_id FROM Reservations
+            WHERE employee_id = 2 and date_time_created = '2024-03-18 10:05:02'),
+            (SELECT participant_id FROM Participants 
+                WHERE first_name = 'Becky' and last_name = 'Hendon' and phone_number = '351-098-3289')),
+        ((SELECT reservation_id FROM Reservations
+            WHERE employee_id = 2 and date_time_created = '2024-02-03 03:03:03'),
+            (SELECT participant_id FROM Participants 
+                WHERE first_name = 'Becky' and last_name = 'Hendon' and phone_number = '351-098-3289')),
+        ((SELECT reservation_id FROM Reservations
+            WHERE employee_id = 2 and date_time_created = '2024-02-03 03:03:03'),
+            (SELECT participant_id FROM Participants 
+                WHERE first_name = 'Andrew' and last_name = 'Kent' and phone_number = '564-798-0908'));
 
 
 -- Print Tables with Sample Data:
 SELECT * FROM Employees;
 SELECT * FROM Participants;
-SELECT * FROM Reservations;
 SELECT * FROM Campgrounds;
 SELECT * FROM Programs;
-
+SELECT * FROM Reservations;
+SELECT * FROM Reservations_has_Participants;
 
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
