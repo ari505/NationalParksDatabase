eec357c3208d64d142a721fc86ef5c86bb8ac6e8
ViviMac5 <mcintyvi@oregonstate.edu>
2024-05-16 22:13:02 -0700

    DML change

--- DML.sql
+++ DML.sql
@@ -29,7 +29,7 @@ employee_id, camping_start_date, camping_end_date)
 --Reservations has Participants
 INSERT INTO Reservations_has_Participants (reservation_id, participant_id)
     VALUES 
-        (#reservation_id, participant_id) --make foreign keys into drop downs later
+        (#reservation_id_from_input, #participant_id_from_input)
 
 --All SELECT Queries
 
@@ -49,13 +49,16 @@ SELECT name, capacity, location, date_time, employee_id FROM Programs
 SELECT date_created, is_campground, campground_id, program_id, 
 employee_id, camping_start_date, camping_end_date FROM Reservations 
 
---SELECT everything from Reservations_has_Participants
-SELECT reservation_id, participant_id from Reservations_has_Participants 
+--JOINS Reservations, Participants, Reservations_has_Participants to display relevant information in Reservations_has_Participants browse table
+SELECT Reservations.reservation_id, Participants.participant_id, Participants.first_name, Participants.last_name, Reservations.program_id, Reservations.campground_id FROM Reservations
+    INNER JOIN Reservations_has_Participants ON Reservations_has_Participants.reservation_id = Reservations.reservation_id
+    INNER JOIN Participants ON Participants.participant_id = Reservations_has_Participants.participant_id
+        WHERE Participants.first_name = 'Andrew' and last_name = 'Kent' and phone_number = '564-798-0908';
 
 --UPDATE and DELETE Queries 
 
---UPDATE in Reservations
-UPDATE Reservations SET reservation_id = #reservation_id WHERE reservation_id = #reservation_id_of_row_chosen_for_update
+--UPDATE in Reservations_has_Paricipants
+UPDATE Reservations_has_Participants SET reservation_id = #reservation_id, participant_id = #participant_id WHERE reservation_id = #reservation_id_of_row_chosen_for_update
 
---DELETE in Reservations
-DELETE FROM Reservations WHERE reservation_id = #reservation_id_of_row_chosen_for_delete
+--DELETE in Reservations_has_Participants
+DELETE FROM Reservations_has_Participants WHERE reservation_id = #reservation_id AND participant_id = #participant_id
