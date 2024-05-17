efe0c64d71b035d00960f8b0f4a571f54cfb8298
ViviMac5 <mcintyvi@oregonstate.edu>
2024-05-16 21:18:35 -0700

    DML changed

--- DML.sql
+++ DML.sql
@@ -16,20 +16,20 @@ INSERT INTO Campgrounds (campground_name, num_campsites)
         (campground_name, num_campsites); 
 
 --Programs
-INSERT INTO Programs (name, capacity, location, date_time, Employee_id)
+INSERT INTO Programs (name, capacity, location, date_time, employee_id)
     VALUES 
-        (#name, #capacity, #location, #date_time, #Employee_id) --make foreign key into drop downs later
+        (#name, #capacity, #location, #date_time, #employee_id) --make foreign key into drop downs later
 
 --Reservations
-INSERT INTO Reservations (date_created, is_campground, Campground_id, Program_id, 
-Employee_id, camping_start_date, camping_end_date) 
+INSERT INTO Reservations (date_created, is_campground, campground_id, program_id, 
+employee_id, camping_start_date, camping_end_date) 
     VALUES
-        (#date_created, #campground_or_program_from_radio, #Campground_id, #Program_id, #Employee_id_from_dropdown, #camping_start_date, #camping_end_date) --make foreign keys into drop down later
+        (#date_created, #campground_or_program_from_radio, #campground_id, #program_id, #employee_id_from_dropdown, #camping_start_date, #camping_end_date) --make foreign keys into drop down later
 
 --Reservations has Participants
-INSERT INTO Reservations_has_Participants (Reservation_id, Participant_id)
+INSERT INTO Reservations_has_Participants (reservation_id, participant_id)
     VALUES 
-        (#Reservation_id, Participant_id) --make foreign keys into drop downs later
+        (#reservation_id, participant_id) --make foreign keys into drop downs later
 
 --All SELECT Queries
 
@@ -43,19 +43,19 @@ SELECT first_name, last_name, age, phone_number, email FROM Participants
 SELECT campground_name, num_campsites FROM Campgrounds;
 
 --SELECT everything from Programs
-SELECT name, capacity, location, date_time, Employee_id FROM Programs 
+SELECT name, capacity, location, date_time, employee_id FROM Programs 
 
 --SELECT everything from Reservations
-SELECT date_created, is_campground, Campground_id, Program_id, 
-Employee_id, camping_start_date, camping_end_date FROM Reservations 
+SELECT date_created, is_campground, campground_id, program_id, 
+employee_id, camping_start_date, camping_end_date FROM Reservations 
 
 --SELECT everything from Reservations_has_Participants
-SELECT Reservation_id, Participant_id from Reservations_has_Participants 
+SELECT reservation_id, participant_id from Reservations_has_Participants 
 
 --UPDATE and DELETE Queries 
 
---UPDATE in Reservations_has_Paricipants
-UPDATE Reservations_has_Participants SET Reservation_id = #Reservation_id, Participant_id = #Participant_id WHERE Reservation_id = #Reservation_id_of_row_chosen_for_update
+--UPDATE in Reservations
+UPDATE Reservations SET reservation_id = #reservation_id WHERE reservation_id = #reservation_id_of_row_chosen_for_update
 
---DELETE in Reservations_has_Participants
-DELETE FROM Reservations_has_Participants WHERE Reservation_id = #Reservation_id AND Participant_id = #Participant_id
+--DELETE in Reservations
+DELETE FROM Reservations WHERE reservation_id = #reservation_id_of_row_chosen_for_delete
