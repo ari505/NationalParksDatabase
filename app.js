a3328a34d2f0e573c2b72e939328797725e99c01
ari505 <114538411+ari505@users.noreply.github.com>
2024-06-04 02:22:23 -0700

    site now fully responsive to screen size changes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static('public'))
 
 
 // Port
-PORT        = 55555;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 55565;                 // Set a port number at the top so it's easy to change in the future
 
 
 // Handlebars
